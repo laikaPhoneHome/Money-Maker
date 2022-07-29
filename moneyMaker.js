50f2e44ba3c99eab3012a18dc464702d3f3a2b4a
Robyn <clockhands9@gmail.com>
2022-07-29 17:41:19 +0100

    My latest money making scheme

---
+++ moneyMaker.js
@@ -0,0 +1,60 @@
+
+
+/* 
+Welcome to the money maker, please input your note value and amount of each at the same index then pick your currency.
+Sadly Ive run out of change. Enjoy.
+*/
+
+
+function moneyMaker(kind, amount, currency){
+let moneyOut = 0;
+
+ for (let m=0; m<kind.length; m++){
+    if (kind[m] === 1 && currency.split(" ")[0] === "dollars"){
+        for (let i=0; i<amount[m]; i++){
+            moneyOut += i;
+
+        }
+    }else if(kind[m] === 5){
+        for (let i=0; i<amount[m]; i++){
+            moneyOut += i * 5;
+
+        }
+    }else if(kind[m] === 10){
+        for (let i=0; i<amount[m]; i++){
+            moneyOut += i * 10;
+
+        }
+    }else if(kind[m] === 20){
+        for (let i=0; i<amount[m]; i++){
+            moneyOut += i * 20;
+
+        }
+    }else if(kind[m] === 50){
+        for (let i=0; i<amount[m]; i++){
+            moneyOut += i * 50;
+
+        }
+    }else if(kind[m] > 50 || kind[m] % 10 !== 0 || kind[m]< 10){
+        return "I only do notes, sorry";
+    }
+
+if (currency === "pounds please"){
+    let moneyMaker = (`KA-CHING! Heres £${moneyOut}, youre welcome.`)
+    return moneyMaker;
+}else if (currency === "dollars please"){
+    let moneyMaker = (`BEEP BEEP! Heres $${moneyOut}, Take it.`)
+    return moneyMaker;
+}else if (currency === "euros please"){
+    let moneyMaker = (`BURRRR! Heres €${moneyOut}, Dont spend it all at one place.`)
+    return moneyMaker;
+}else if(currency.split(" ")[1] !== "please"){
+    return "say please";
+}else if(!currency){
+    return "what currency?";
+}else
+    return "I dont do that one";
+ }
+}
+
+console.log(moneyMaker(20,20,'pounds please'));
